6dba1ee4ffb3eb475ee354d0ba517e6070604872
Aniketgupta4 <aniketkumargupta302@gmail.com>
2025-08-26 20:04:54 +0530

    tailwind

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
    <>
- <footer className="bg-teal-500 text-white p-4 w-full fixed bottom-0 left-0">
+ <footer className="bg-teal-500 text-white p-4 w-full m-4 fixed bottom-0 left-0">
   <div className="max-w-6xl mx-auto flex justify-center items-center">
     <h1 className="text-center">
       aniket302@gmail.com | 8770191XXX
