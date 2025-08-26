b05ae522e78ffc5ad95beb88fa36092e881e1e01
Aniketgupta4 <aniketkumargupta302@gmail.com>
2025-08-26 19:35:38 +0530

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,28 +1,110 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App(){
     return(
         <>
 
          <Header></Header>
+          
+        <div className="flex flex-wrap justify-center gap-10 pb-30">
 
-         <div className="max-w-sm overflow-hidden bg-teal-200 shadow-lg rounded-lg">
+         <div className="max-w-sm overflow-hidden bg-teal-300 shadow-lg rounded-lg">
             <div className="m-4 p-4">
                 <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/d5/21/78/d52178c08c4d103f2908a97a00be0443.jpg"></img>
             </div>
-            <div className="">
-                <h1>Biography</h1>
+            <div className="ml-4 mr-4 mb-2 font-bold text-2xl">
+                <h1>Rashmika Mandana</h1>
             </div>
-            <div>
-                <p>Archi is so cute</p>
+            <div className="ml-4 mr-4 mb-2 text-gray-500 text-md">
+                <p>She is an Actor, She is Emotional, Charming and Cute.</p>
             </div>
-            <div>
-                <button>Know more</button>
+            <div className="m-4">
+                <button className="bg-teal-800 text-white rounded-xl p-2 hover:bg-teal-500">Know more</button>
+            </div>
+         </div>
+
+           <div className="max-w-sm overflow-hidden bg-teal-300 shadow-lg rounded-lg">
+            <div className="m-4 p-4">
+                <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/d5/21/78/d52178c08c4d103f2908a97a00be0443.jpg"></img>
+            </div>
+            <div className="ml-4 mr-4 mb-2 font-bold text-2xl">
+                <h1>Rashmika Mandana</h1>
+            </div>
+            <div className="ml-4 mr-4 mb-2 text-gray-500 text-md">
+                <p>She is an Actor, She is Emotional, Charming and Cute.</p>
+            </div>
+            <div className="m-4">
+                <button className="bg-teal-800 text-white rounded-xl p-2 hover:bg-teal-500">Know more</button>
+            </div>
+         </div>
+
+           <div className="max-w-sm overflow-hidden bg-teal-300 shadow-lg rounded-lg">
+            <div className="m-4 p-4">
+                <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/d5/21/78/d52178c08c4d103f2908a97a00be0443.jpg"></img>
+            </div>
+            <div className="ml-4 mr-4 mb-2 font-bold text-2xl">
+                <h1>Rashmika Mandana</h1>
+            </div>
+            <div className="ml-4 mr-4 mb-2 text-gray-500 text-md">
+                <p>She is an Actor, She is Emotional, Charming and Cute.</p>
+            </div>
+            <div className="m-4">
+                <button className="bg-teal-800 text-white rounded-xl p-2 hover:bg-teal-500">Know more</button>
+            </div>
+         </div>
+
+           <div className="max-w-sm overflow-hidden bg-teal-300 shadow-lg rounded-lg">
+            <div className="m-4 p-4">
+                <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/d5/21/78/d52178c08c4d103f2908a97a00be0443.jpg"></img>
+            </div>
+            <div className="ml-4 mr-4 mb-2 font-bold text-2xl">
+                <h1>Rashmika Mandana</h1>
+            </div>
+            <div className="ml-4 mr-4 mb-2 text-gray-500 text-md">
+                <p>She is an Actor, She is Emotional, Charming and Cute.</p>
+            </div>
+            <div className="m-4">
+                <button className="bg-teal-800 text-white rounded-xl p-2 hover:bg-teal-500">Know more</button>
+            </div>
+         </div>
+
+           <div className="max-w-sm overflow-hidden bg-teal-300 shadow-lg rounded-lg">
+            <div className="m-4 p-4">
+                <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/d5/21/78/d52178c08c4d103f2908a97a00be0443.jpg"></img>
+            </div>
+            <div className="ml-4 mr-4 mb-2 font-bold text-2xl">
+                <h1>Rashmika Mandana</h1>
+            </div>
+            <div className="ml-4 mr-4 mb-2 text-gray-500 text-md">
+                <p>She is an Actor, She is Emotional, Charming and Cute.</p>
+            </div>
+            <div className="m-4">
+                <button className="bg-teal-800 text-white rounded-xl p-2 hover:bg-teal-500">Know more</button>
+            </div>
+         </div>
+
+           <div className="max-w-sm overflow-hidden bg-teal-300 shadow-lg rounded-lg">
+            <div className="m-4 p-4">
+                <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/d5/21/78/d52178c08c4d103f2908a97a00be0443.jpg"></img>
+            </div>
+            <div className="ml-4 mr-4 mb-2 font-bold text-2xl">
+                <h1>Rashmika Mandana</h1>
+            </div>
+            <div className="ml-4 mr-4 mb-2 text-gray-500 text-md">
+                <p>She is an Actor, She is Emotional, Charming and Cute.</p>
+            </div>
+            <div className="m-4">
+                <button className="bg-teal-800 text-white rounded-xl p-2 hover:bg-teal-500">Know more</button>
             </div>
          </div>
         
+        </div>
+    
+        <Footer></Footer>
+
         </>
     ) 
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+ <footer className="bg-teal-500 text-white p-4 w-full fixed bottom-0 left-0">
+  <div className="max-w-6xl mx-auto flex justify-center items-center">
+    <h1 className="text-center">
+      aniket302@gmail.com | 8770191XXX
+    </h1>
+  </div>
+ </footer>
+   </>
+  )
+}
+
+export default Footer
